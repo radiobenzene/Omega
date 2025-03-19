400b43978ad765cfd5d3c15ca04a25321c5562b8
radiobenzene <aurangabadkaruditangshu@gmail.com>
2025-03-19 10:22:39 +0000

    fix: fix function header

--- monoid.m
+++ monoid.m
@@ -1,9 +1,18 @@
-% Function that resemebles sigmoid
-% MONOID - My sigOID
+% Sigmoid function with the parameters x0 and K
+% Input:
+%   x : values
+%   x0 : axis-shift parameter
+%   K : axis-shift parameter
+% Output:
+%   res: result of sigmoid function
 function [res] = monoid(x, x0, K)
+    % Specifying the numerator
     numerator = 1;
-    denominator = (1 + exp(K * (x - x0)));
 
+    % Specifying the denominator
+    denominator = (1 + exp(K * (x - x0)));
+    
+    % Resulting sigmoid function
     res = numerator / denominator;
     
 end
