6c71b4a6dfc153350e8713f4c87e2613aa819d3c
radiobenzene <aurangabadkaruditangshu@gmail.com>
2025-03-19 09:34:57 +0000

    upd: add documentation to function

--- getAlpha.m
+++ getAlpha.m
@@ -1,3 +1,9 @@
+% Function to measure alpha, i.e how 
+% Input:
+%   Q_restored : Q for the restored image
+%   Q_original : Q for the original image
+% Output:
+%   del_value : alpha
 function [del_value] = getAlpha(Q_restored, Q_original)
     % Taking absolute value
     numerator = abs(Q_restored - Q_original);
